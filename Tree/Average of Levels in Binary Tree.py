8a0b4968f77e8db1371b8688a4a5abc2067516eb
Adarsh Gupta <73924145+adarshgupta0040@users.noreply.github.com>
2022-09-02 23:12:45 +0530

    Create Average of Levels in Binary Tree.py

---
+++ Tree/Average of Levels in Binary Tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return 
+        else:
+            queue=[]
+            ans=[]
+            queue.append(root)
+            while(queue):
+                temp=[]
+                for i in range(len(queue)): 
+                    node=queue.pop(0)
+                    temp.append(node.val)
+                    if node.left !=None:
+                        queue.append(node.left)
+                    if node.right !=None:
+                        queue.append(node.right)
+                ans.append(temp)
+        return (ans)
