7b95b92e2f8629f172215bc1c7b180d03cb47d57
Adarsh Gupta <73924145+adarshgupta0040@users.noreply.github.com>
2022-09-10 16:29:52 +0530

    Create Split a String in a Balance String.py

---
+++ Strings/Split a String in a Balance String.py
@@ -0,0 +1,12 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        count=0
+        res=0
+        for i in s:
+            if i=='L':
+                count+=1
+            else:
+                count-=1
+            if count==0:
+                res+=1
+        return res
