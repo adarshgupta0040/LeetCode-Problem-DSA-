377ac32907b9f782ad278f349c0dfcf65c790e86
Adarsh Gupta <73924145+adarshgupta0040@users.noreply.github.com>
2022-10-09 18:34:20 +0530

    Update Check Balanced Binary Tree.py

--- Tree/Check Balanced Binary Tree.py
+++ Tree/Check Balanced Binary Tree.py
@@ -26,3 +26,33 @@ class Solution:
         if self.height(root)== -1 :      #if height is -1 then False else True
             return False
         return True
+ 
+                                                   OR
+    
+    
+    # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def height(self,root):
+        if root==None:
+            return 0
+        lh = self.height(root.left)
+        if lh==-1:
+            return -1
+        rh = self.height(root.right)
+        if rh==-1:
+            return -1
+        if abs(lh-rh)>1:
+            return -1
+        return 1+max(lh,rh)
+        
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root==None:
+            return True
+        if self.height(root)== -1:
+            return False
+        return True    
